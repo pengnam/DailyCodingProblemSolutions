a23ff0caf78fdc33159ca6d3655dbc566f43886c
pengnam <seanngpengnam@gmail.com>
2019-03-09 10:16:09 +0800

    Add problem 6

---
+++ Problem6-XORLinkedList.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+    def append(self, next_node):
+        #assumption: called only once
+        self.both = next_node.get_pointer() ^ self.both
+    def get(self, prev):
+        return self.both ^ prev
+
+
+class LinkedList:
+    def __init__(self):
+        self.start = None
+    def add(self, element):
+        if not self.start:
+            self.start = get_pointer(element)
+            self.end = self.start
+
+        dereference_pointer(self.end).append(element)
+        self.end = get_pointer(element)
+
+    def get(self, index):
+        current = self.start
+        for i in range(index):
+            current = dereference_pointer(self.start).get(current)
+        return current
+
+
